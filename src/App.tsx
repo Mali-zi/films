41fdd98a1bb66d74c54012e405d9414c6a810396
Mali-zi <mne310872@yandex.ru>
2023-06-01 00:35:20 +0300

    feat: create Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
 import './App.css';
 import Stars from './components/Stars';
 
-function App() {
-  return (
-      <Stars count={1}/>
-  );
+function App(): JSX.Element {
+  type StarCount = 1 | 2 | 3 | 4 | 5;
+  const count: StarCount = 3;
+  if (typeof count === 'StarCount') {
+    return (
+      <div>
+        <Stars count={count}/>
+      </div>
+    );
+  };
 }
 
 export default App;
